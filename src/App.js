0ad94d073a98827bef069c82891a82ca2539388c
Adrian <adunham95@gmail.cooom>
2020-03-09 06:48:38 -0400

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Route, useLocation, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* <Header/> */}
+        {/* <ScrollToTop/> */}
+        <ModalSwitch/>
+        {/* <Nav /> */}
+      </Router>
+    <div id="modal-root"></div>
     </div>
   );
+}
+
+function ModalSwitch(){
+  let location = useLocation();
+  let background = location.state && location.state.background;
+  console.log("background",background);
+
+  return (
+    <div>
+      <Switch location={background || location}>
+        {/* <Route exact path="/" children={<MainPage />} /> */}
+        {/* <Route path="/new/:type" children={<NewGame />} /> */}
+      </Switch>
+
+      {/* Show the modal when a background page is set */}
+      {/* {background && <Route path="/new/:type" children={<ModalPage/>} />} */}
+    </div>
+  );
+  
 }
 
 export default App;
