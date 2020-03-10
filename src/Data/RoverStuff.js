09964594db8c0c99a46cf9aa2dcd318e003fd797
Adrian Dunham <adrian_dunham@discovery.com>
2020-03-10 12:58:50 -0400

    Added energy, motors, wheels, and updated the names

--- src/Data/RoverStuff.js
+++ src/Data/RoverStuff.js
@@ -1,20 +1,37 @@
 export const roverTypes = [
-    {type_id: "atv", type_name: "ATV", lifeSupport: false, seats: 1, slots: 0, type: "Land"},
-    {type_id: "mini", type_name: "Mini", lifeSupport: false, seats: 0, slots: 0, type: "Land"},
-    {type_id: "exSmall", type_name: "Extra Small", lifeSupport: true, seats: 1, slots: 1, type: "Land"},
-    {type_id: "small",type_name: "Small", lifeSupport: true, seats: 1, slots: 2, type: "Land"},
-    {type_id: "medium",type_name: "Medium", lifeSupport: true, seats: 2, slots: 4, type: "Land"},
-    {type_id: "large",type_name: "Large", lifeSupport: true, seats: 2, slots: 6, type: "Land"},
-    {type_id: "exLarge",type_name: "Extra Large", lifeSupport: true, seats: 2, slots: 8, type: "Land"},
+    {type_id: "atv_land", type_name: "ATV", lifeSupport: false, 
+    seats: 1, slots: 1,storage:0,energy:5,motors:1,wheels:4,type: "Land"},
+
+    {type_id: "moto_land", type_name: "Motorcycle", lifeSupport: false, 
+    seats: 1, slots: 0,storage:0,energy:5,motors:1,wheels:2,type: "Land"},
+
+    {type_id: "exSmall_land", type_name: "Extra Small", lifeSupport: true, 
+    seats: 2, slots: 1,storage:0,energy:5,motors:1,wheels:4,type: "Land"},
+
+    {type_id: "small_land",type_name: "Small", lifeSupport: true, 
+    seats: 1, slots: 3,storage:0,energy:10,motors:2,wheels:4,type: "Land"},
+
+    {type_id: "medium_land",type_name: "Medium", lifeSupport: true, 
+    seats: 2, slots: 5,storage:0,energy:15,motors:2,wheels:4,type: "Land"},
+
+    {type_id: "large_land",type_name: "Large", lifeSupport: true, 
+    seats: 2, slots: 7,storage:0,energy:30,motors:4,wheels:6,type: "Land"},
+
+    {type_id: "exLarge_land",type_name: "Extra Large", lifeSupport: true, 
+    seats: 2, slots: 10,storage:0,energy:50,motors:6,wheels:6,type: "Land"},
 ]
 
 export const upgrades = [
     {name: "Autonomous"},
-    {name: "Hitch"}
+    {name: "Hitch"},
+    {name: "Winch"}
 ]
 
 export const roverSlots = [
-    {slot_id: "crewCab", blockName: "Crew Cab", seats: 4, slotCost: 2, storage: 0},
-    {slot_id: "truckBed", blockName: "Truck Bed", seats: 0, slotCost: 2, storage: 4},
+    {slot_id: "crewCab", blockName: "Crew Cab", seats: 4, slotCost: 2},
+    {slot_id: "truckBed", blockName: "Truck Bed", slotCost: 2, storage: 4},
     {slot_id: "mobileHab", blockName: "Mobile Hab", seats: 1, slotCost:4, storage: 3},
+    {slot_id: "battery_s", blockName: "Small Battery", slotCost:1, energy: 5},
+    {slot_id: "battery_m", blockName: "Medium Battery", slotCost:2, energy: 15},
+    {slot_id: "battery_l", blockName: "Large Battery", slotCost:3, energy: 30},
 ]
